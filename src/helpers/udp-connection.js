ab51715e0f61ff0f0fe5ba9bc1cda85c912ba4fb
Andreas Antener <antener_a@gmx.ch>
2021-09-22 13:09:34 +0200

    udp-connection: provide remote and local host in constructor

--- src/helpers/udp-connection.js
+++ src/helpers/udp-connection.js
@@ -4,7 +4,7 @@ var util = require("util");
 var dgram = require('dgram');
 var loggingBase = require('../lib/logging-base.js')
 
-var UdpConnection = function(localAddr, localPort, client) {
+var UdpConnection = function(localAddr, localPort = 14570, remoteHost, remotePort = 14555) {
     var self = this;
 
     self.logger = new loggingBase.LoggingFactory().getLogger();
@@ -12,27 +12,23 @@ var UdpConnection = function(localAddr, localPort, client) {
     self.config = {
         localPort: localPort,
         localAddr: localAddr,
-        remotePort: undefined,
-        remoteAddr: undefined
+        remotePort: remoteHost,
+        remoteAddr: remotePort
     };
 
     self.socket = dgram.createSocket('udp4');
 
-    if (!client) {
-        self.socket.bind(self.config.localPort, function(){
+    if (self.config.localAddr !== undefined && self.config.localPort !== undefined) {
+        self.socket.bind(self.config.localPort, self.config.localAddr, function(){
             self.logger.info(util.format("Bound local interface %s:%s", self.config.localAddr, self.config.localPort));
         });
-
-    } else {
-        self.config.remotePort = localPort;
-        self.config.remoteAddr = localAddr;
     }
 
     self.socket.on("message", function(data, remote){
         self.emit("data", data);
 
-        if (self.config.remoteAddr != remote.address || self.config.remoteAddr != remote.address) {
-            self.logger.info(util.format("Switching to remote %s:%d", remote.address, remote.port));
+        if (self.config.remoteAddr === undefined) {
+            self.logger.info(util.format("New remote %s:%d", remote.address, remote.port));
 
             self.config.remotePort = remote.port;
             self.config.remoteAddr = remote.address;
@@ -43,7 +39,7 @@ var UdpConnection = function(localAddr, localPort, client) {
 UdpConnection.prototype.send = function(buffer){
     var self = this;
 
-    if (self.socket && self.config.remoteAddr) {
+    if (self.socket && self.config.remoteAddr && self.config.remotePort) {
         self.socket.send(buffer, 0, buffer.length
             , self.config.remotePort, self.config.remoteAddr);
     }
